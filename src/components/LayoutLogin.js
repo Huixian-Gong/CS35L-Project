4f46b45b55226fccde2e722a221fafbd9a8ba3bc
Huixian Gong <hx@g.ucla.edu>
2022-12-01 14:31:32 -0800

    Reformatted the login page background image

--- src/components/LayoutLogin.js
+++ src/components/LayoutLogin.js
@@ -1,11 +1,11 @@
 import React from "react";
 import LoginNavigation from "./LoginNavigation";
 import style from './LayoutLogin.module.css';
-
+import img from '../ui/skyline.jpg'
 function Layout (props) {
     return (
     <div  style={{ 
-        backgroundImage: `url("https://shb.ais.ucla.edu/shibboleth-idp/img/skyline.jpg")` ,
+        backgroundImage: `url(${img})` ,
         backgroundSize:"cover",
         height: "100vh",
         backgroundPosition: 'center',
