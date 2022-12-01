c661481770ae1713a456c5f5eadc27a1d0da81eb
RickyYan53 <ruiqiyan@g.ucla.edu>
2022-12-01 00:20:32 -0800

    -Add password confirmation feature during sign up -Debugged the sign up feature

--- nraelniurb/src/components/SignupForm.js
+++ nraelniurb/src/components/SignupForm.js
@@ -6,19 +6,35 @@ import style from './SignupForm.module.css'
 const Signup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [cpassword, setCpassword] = useState('');
   const [error, setError] = useState('')
   const { createUser } = UserAuth();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [match, setMatch] = useState(true);
+
+  // function handleMatch (e) {
+  //   e.preventDefault();
+  //   if(password === cpassword){
+  //     setMatch(true);
+  //   }
+  //   console.log("error")
+  // }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError('');
-    try {
-      await createUser(email, password);
-      navigate('/login')
-    } catch (e) {
-      setError(e.message);
-      console.log(e.message);
+    if(password === cpassword) {
+      try {
+        await createUser(email, password);
+        navigate('/login')
+      } catch (e) {
+        setError(e.message);
+        console.log(e.message);
+      }
+      setMatch(true)
+    }
+    else {
+      setMatch(false)
     }
   };
 
@@ -30,6 +46,7 @@ const Signup = () => {
         <h2>Sign up</h2>
         {(error == "Firebase: Error (auth/invalid-email).") ? ( <div className={style.error}>{"Error, Please enter valid email"}</div> ) : ""}
         {(error == "Firebase: Password should be at least 6 characters (auth/weak-password).") ? ( <div className={style.error}>{"Error, password should be at least 6 characters"}</div> ) : ""}
+        {match ? "" : ( <div className={style.error}>{"Error, passwords doesn't match"}</div> )}
           <label className={style.group}>Email Address</label>
           <input
             onChange={(e) => setEmail(e.target.value)}
@@ -38,10 +55,16 @@ const Signup = () => {
           />
           </div>
           <div className={style.group}>
-            <label className='py-2 font-medium'>Password</label>
+            <label>Password</label>
             <input
               onChange={(e) => setPassword(e.target.value)}
-              className='border p-3'
+              type='password'
+            />
+          </div>
+          <div className={style.group}>
+            <label>Confirm Password</label>
+            <input
+              onChange={(e) => setCpassword(e.target.value)}
               type='password'
             />
           </div>
